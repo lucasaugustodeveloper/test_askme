68e8d620b781aba07adbe76c5cd9858f21118bee
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-08-27 22:15:33 -0300

    Add new components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,18 @@
-import React from "react";
 import "./App.css";
 
+import React from "react";
+import Header from "../../components/header";
+import OnlyContent from "../../components/onlyContent";
+import Benefits from "../../components/benefits";
+
 function App() {
-  return <div className="App">incentivar</div>;
+  return (
+    <div className="container-fluid">
+      <Header />
+      <OnlyContent />
+      <Benefits />
+    </div>
+  );
 }
 
 export default App;
